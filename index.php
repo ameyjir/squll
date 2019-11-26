546edf2cdb5a2e1c315276f784f64883e32085f5
ameyjir <52775274+ameyjir@users.noreply.github.com>
2019-11-26 16:38:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<title>Remote upload made by wRock.Org</title>
+<center>
+<a href="https://www.squll.tk" title="Squll">
+			
+		</a>
+</br</p></br</p><form method="post">
+<input name="url" size="50" />
+<input name="submit" type="submit" />
+</form>
+
+<b>Instruction:</b>
+</p>You can use this uplaoder to uplaod data from any DIRECT LINK.
+</p><b> • From FTP</b> ftp://username:password@yourdomain.com/path/to/file.extension
+</p><b> • From HTTP/ web</b> http://example.com/path/to/file.extension
+</p>
+</p> Made with love by <a  href="http://www.wrock.org/">Squll</a>
+<?php
+
+// maximum execution time in seconds
+set_time_limit (24 * 60 * 60);
+
+if (!isset($_POST['submit'])) die();
+
+// folder to save downloaded files to. must end with slash
+$destination_folder = 'files/';
+
+$url = $_POST['url'];
+$newfname = $destination_folder . basename($url);
+
+$file = fopen ($url, "rb");
+if ($file) {
+  $newf = fopen ($newfname, "wb");
+
+  if ($newf)
+  while(!feof($file)) {
+    fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
+  }
+}
+
+if ($file) {
+  fclose($file);
+}
+
+if ($newf) {
+  fclose($newf);
+}
+
+?>
+</center>
